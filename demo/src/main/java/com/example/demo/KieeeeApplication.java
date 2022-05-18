20ddf8d250a4d565a6efe97d82b98fb77eb62d93
juanjuwu <jjlaramelcher@gmail.com>
2022-05-17 20:00:42 -0400

    primer test

---
+++ demo/src/main/java/com/example/demo/KieeeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieeeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieeeeApplication.class, args);
+	}
+
+}
